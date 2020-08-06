30b3c4e03220ba92dc35f5bcf4483bfb06501f89
igorujevic <iujevi00@fesb.hr>
2020-08-06 16:51:02 +0200

    first commit - project done

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+});
+
+export const db = firebase.firestore();
+
+new Vue({
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
